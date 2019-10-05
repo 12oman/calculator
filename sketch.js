893d5777abccfdd36d7042d47bd70f41b408146c
Roman Mitch <romanmitch@gmail.com>
2019-10-06 10:00:16 +1300

    add p5 sketch and dpendencies

--- sketch.js
+++ sketch.js
@@ -3,3 +3,53 @@
 // set the background to clear so canvas doesn't appear
 // draw loop, create bubbles to match result from calculator
 //  
+var bubObjs = [];
+var result = 1000;
+var screen;
+// will need to change this variable from calculator result.
+
+function setup() {
+
+    screen = createCanvas(windowWidth, windowHeight);
+    screen.position(0,0);
+    screen.style('z-index', '-1');
+
+	//bubble array calling the bubble constructor
+	for (i = 0; i < result; i++) {
+		bubObjs[i] = new Bubble();
+	}
+
+}
+
+// this draw function is actually a loop from the p5.js library
+function draw() {
+    background(150,150,255);
+    // clear();
+    for (i = 0; i < bubObjs.length; i++) {
+    bubObjs[i].display();
+	bubObjs[i].move();
+	}
+	
+
+}
+
+
+
+
+function Bubble() {
+	this.x = random(windowWidth *.2, windowHeight *.6 );
+  	this.y = random(windowWidth *.2,windowHeight *.6);
+  	// this.h = random(20,25)
+	// this.w = random(20,25)
+	this.display = function() {
+    stroke(250, 250, 255);
+    strokeWeight(2);
+    // strokeColor(0,0,0);
+	noFill();
+	ellipse(this.x, this.y, 20, 20);
+		},
+		this.move = function() {
+			this.x += random(-10,10);
+			this.y += random(-10,10);
+	}
+}
